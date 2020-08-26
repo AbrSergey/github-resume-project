b1fa3f1eaadeff2bb06cf9677e37f000921d20e1
Sergey <sergey.abramov996@gmail.com>
2020-08-26 17:39:45 +0700

    refactor env

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export default {
   backend: {
-    host: process.env.BACKEND_HOST // 'https://hh-test-backend.herokuapp.com'
+    host: 'https://hh-test-backend.herokuapp.com', //process.env.BACKEND_HOST
   },
   github: {
-    auth_url: process.env.GITHUB_AUTH_URL, //'https://github.com/login/oauth/authorize',
+    auth_url: 'https://github.com/login/oauth/authorize', //process.env.GITHUB_AUTH_URL,
     client_id: process.env.GITHUB_CLIENT_ID,
     client_secret: process.env.GITHUB_CLIENT_SECRET,
     redirect_url: process.env.GITHUB_REDIRECT_URL, //'http://localhost:3000/token',
-    base_url: process.env.GITHUB_BASE_URL //'https://api.github.com'
+    base_url: 'https://api.github.com' //process.env.GITHUB_BASE_URL
   }
 };
