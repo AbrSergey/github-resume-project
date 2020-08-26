caef1186c4f448ae1074dfac9fb9206aa74f3c8d
Sergey <sergey.abramov996@gmail.com>
2020-08-26 17:59:28 +0700

    refactor env

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export default {
     auth_url: 'https://github.com/login/oauth/authorize', //process.env.GITHUB_AUTH_URL,
     client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
     client_secret: process.env.REACT_APP_GITHUB_CLIENT_SECRET,
-    redirect_url: process.env.REACT_APP_GITHUB_REDIRECT_URL, //'http://localhost:3000/token',
+    redirect_url: process.env.REACT_APP_REDIRECT_URL, //'http://localhost:3000/token',
     base_url: 'https://api.github.com' //process.env.GITHUB_BASE_URL
   }
 };
